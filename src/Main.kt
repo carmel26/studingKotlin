5d461f3dcf3b00507fb35c5b64f330b96c626fa9
carmel26 <nkeshipelagie@gmail.com>
2025-04-15 10:52:30 +0300

    function with parameters on 15Ap2025 at 10h52

--- src/Main.kt
+++ src/Main.kt
@@ -1,20 +1,14 @@
 
 var name : String? = null // mutable (changeable)
-val greeting: String? = null // immutable (unchangeable)
+val greetingValue: String? = null // immutable (unchangeable)
 
-fun getGreetings() : String {
-    return "Hello , It is a new day !"
+fun sayHallo(greeting : String, itemToGreet : String) {
+    val message = "$greeting $itemToGreet"
+    println(message)
 }
 
-// single expression and we can also remove the type
-fun singleExpression() : String = "We are in MweCaU"
-
-// unit means return nothing usable we can even remove it
-fun sayHallo() : Unit {
-    println(getGreetings())
-}
 fun main() {
      name = "Kotlin"
-     sayHallo()
-     println(singleExpression())
+    sayHallo("Hello ", "Word!")
+    sayHallo("Hey ", " my Friend")
 }
