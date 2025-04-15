304d32565bd00238ed2de18859983abf8ae3abea
carmel26 <nkeshipelagie@gmail.com>
2025-04-15 16:45:03 +0300

    End working on sealed classes on 15Apr2025

--- src/EntityFactory.kt
+++ src/EntityFactory.kt
@@ -1,7 +1,7 @@
 import java.util.UUID
 
 enum class EntityType{
-    EASY, MEDIUM, HARD;
+    HELP, EASY, MEDIUM, HARD;
 
     fun getFormattedName() = name.lowercase().capitalize()
 }
@@ -12,22 +12,35 @@ object EntityFactory {
         val name = when(type){
             EntityType.EASY -> type.name
             EntityType.MEDIUM -> type.getFormattedName()
-            EntityType.HARD -> type.name
+            EntityType.HARD -> "HaRd"
+            EntityType.HELP -> type.getFormattedName()
         }
-        return Entity(id, name)
+        return when(type){
+            EntityType.EASY -> Entity.Easy(id, name)
+            EntityType.MEDIUM -> Entity.Medium(id, name)
+            EntityType.HARD -> Entity.Hard(id, name, multiplier = 2f)
+            EntityType.HELP ->Entity.Help
+        }
     }
 }
 
-class Entity  (val id: String, val name: String) {
-    override fun toString(): String {
-        return "id:$id name:$name"
+sealed class Entity  () {
+    object Help : Entity(){
+        val name = "Help"
     }
+    data class Easy(val id :String, val name : String): Entity()
+    data class Medium(val id :String, val name : String): Entity()
+    data class Hard(val id :String, val name : String, val multiplier: Float): Entity()
 }
 
 fun main(){
-    val entity = EntityFactory.create(EntityType.EASY)
-    println(entity)
+    val entity : Entity = EntityFactory.create(EntityType.HARD )
+    val msg = when (entity){
+        is Entity.Easy -> "Easy Class"
+        is Entity.Hard -> "Hard Class"
+        Entity.Help ->  "Help Class"
+        is Entity.Medium -> "Medium Class"
+    }
+    println(msg)
 
-    val medium = EntityFactory.create(EntityType.MEDIUM)
-    println(medium)
 }
