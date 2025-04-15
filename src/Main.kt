e6863fb6811b7fd4aa46ba2f6584402f89e798eb
carmel26 <nkeshipelagie@gmail.com>
2025-04-15 10:46:35 +0300

    start woking with function, type of unit, single expression, and so on

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,20 @@
 
 var name : String? = null // mutable (changeable)
 val greeting: String? = null // immutable (unchangeable)
+
+fun getGreetings() : String {
+    return "Hello , It is a new day !"
+}
+
+// single expression and we can also remove the type
+fun singleExpression() : String = "We are in MweCaU"
+
+// unit means return nothing usable we can even remove it
+fun sayHallo() : Unit {
+    println(getGreetings())
+}
 fun main() {
      name = "Kotlin"
-     val greetingToPrint = if(greeting != null) name else " Hi"
-     var newGreetingVal = when(name){
-         null -> " How are you?"
-         else -> name
-     }
-     println("Hello, $greetingToPrint!")
-
+     sayHallo()
+     println(singleExpression())
 }
