08f875c7a4484789720363d919a92a9dc12e478b
carmel26 <nkeshipelagie@gmail.com>
2025-04-15 17:32:56 +0300

    adding filter on foreach 15Ap2025

--- src/AdvancedFunction.kt
+++ src/AdvancedFunction.kt
@@ -1,13 +1,30 @@
-fun printFilterStrings (list: List<String>, predicate: (String) -> Boolean){
+fun printFilterStrings (list: List<String>, predicate: ((String) -> Boolean)?){
     list.forEach{
-        if (predicate(it)){
+        if (predicate?.invoke(it) == true){
             println(it)
         }
     }
 }
 
+val predicate: (String) -> Boolean = {
+    it.startsWith("C")
+}
+
+fun getPrintedPredicate(): (String) -> Boolean {
+    return {it.startsWith("J")}
+}
 
 fun main() {
-    val lists = listOf("Kotlin", "Java", "C++", "C#")
-    printFilterStrings(lists, {it.startsWith("J")})
+    val lists = listOf("Kotlin", "Java", "C++", "C#", "Javascript", null)
+    lists
+        .filterNotNull()
+        .filter{
+            it.startsWith("C")
+        }
+        .map{
+            it.length
+        }
+        .forEach {
+        println(it)
+    }
 }
