94b1628b17651ee67031489bc834fa97b5678996
carmel26 <nkeshipelagie@gmail.com>
2025-04-15 10:17:47 +0300

    adding condition 'if' and 'when' on 15042025

--- src/Main.kt
+++ src/Main.kt
@@ -3,6 +3,17 @@ var name : String? = null // mutable (changeable)
 val greeting: String? = null // immutable (unchangeable)
 fun main() {
      name = "Kotlin"
-    println("Hello, $name!")
 
+    // if condition
+    if (name != null){
+        println("Hello, $name!")
+    }else{
+        println("Hi")
+    }
+
+   // when statement
+    when(greeting) {
+        null -> println("Hi, this is when")
+        else -> println("Greeting from MweCaU!")
+    }
 }
